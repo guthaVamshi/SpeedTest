ae23863ab2d01a1c00ec61ba788229ff6385d765
Vamshi gutha <79559118+guthaVamshi@users.noreply.github.com>
2022-09-16 14:41:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const Block = document.getElementById('Source');
 var selector = 0;
 var image = document.getElementById('Imageset');
 function Changeplug(){
-    var txt = document.getElementById("Source").src ="http://openspeedtest.com/Get-widget.php";
+    var txt = document.getElementById("Source").src ="https://openspeedtest.com/Get-widget.php";
 }
 function Changeplugmeter() {
     var txt = document.getElementById("Source").src ="https://www.metercustom.net/plugin/";
