0a061fc4e81a924623d9cb3c3afa38bda42d1332
Vamshi gutha <79559118+guthaVamshi@users.noreply.github.com>
2022-09-16 14:32:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const Block = document.getElementById('Source');
+var selector = 0;
+var image = document.getElementById('Imageset');
+function Changeplug(){
+    var txt = document.getElementById("Source").src ="http://openspeedtest.com/Get-widget.php";
+}
+function Changeplugmeter() {
+    var txt = document.getElementById("Source").src ="https://www.metercustom.net/plugin/";
+}
+
+function ChangeSRC() {
+    if(selector == 1){
+        var txt = document.getElementById("Source").src ="http://openspeedtest.com/Get-widget.php";
+    }if(selector == 2){
+        var txt = document.getElementById("Source").src ="https://www.metercustom.net/plugin/";
+    }
+}
