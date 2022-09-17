5d476a5c569e65aef465419d79407a3606a0a31b
guthavamshi <gutha.vamshi.11801585@gmail.com>
2022-09-17 08:58:57 +0530

    update

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-const Block = document.getElementById('Source');
-var selector = 0;
-var image = document.getElementById('Imageset');
-function Changeplug(){
-    var txt = document.getElementById("Source").src ="https://openspeedtest.com/Get-widget.php";
-}
-function Changeplugmeter() {
-    var txt = document.getElementById("Source").src ="https://www.metercustom.net/plugin/";
-}
-
-function ChangeSRC() {
-    if(selector == 1){
-        var txt = document.getElementById("Source").src ="http://openspeedtest.com/Get-widget.php";
-    }if(selector == 2){
-        var txt = document.getElementById("Source").src ="https://www.metercustom.net/plugin/";
+/*var limit = 1;
+var toast = document.querySelector('.toast-body');
+$.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/facts?limit=' + limit,
+    headers: { 'X-Api-Key': '7PZbGPZ2fWRutbB/LZYc/Q==vpiX6muvVcNRyhRU'},
+    contentType: 'application/json',
+    success: function(result) {
+        console.log(result);
+        toast.innerHTML = result[0]['fact'];
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
     }
-}
+}); */
